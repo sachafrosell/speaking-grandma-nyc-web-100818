4baea4425de87f4ab5ba1cf8600cfee4cfb77608
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 18:02:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,14 @@
+
+    
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    return 'I LOVE YOU TOO PUMPKIN!'
+  elsif string == string.downcase
+    return 'HUH?! SPEAK UP, SONNY!'
+  else string == string.upcase
+    return 'NO, NOT SINCE 1938!'
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
