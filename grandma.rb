c4f6957078ff2304c4bb962257c0e08d4ca55833
Alexander Frosell <sachafrosell@gmail.com>
2018-10-03 18:11:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,6 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
     else
   		return "NO, NOT SINCE 1938!"
     end
